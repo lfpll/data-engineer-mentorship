9db27d9c9dda9b8ad76dd709131287a4075c32bc
Luiz Lobo <luizfpll@gmail.com>
2021-05-13 08:28:14 -0300

    adicionando novas tabelas

--- databases/sql/create_tables.sql
+++ databases/sql/create_tables.sql
@@ -1,4 +1,4 @@
-CREATE TABLE public.customers (
+CREATE TABLE kimball.customers (
 	customer_id text NULL UNIQUE PRIMARY KEY,
 	customer_unique_id text NOT NULL,
 	customer_zip_code_prefix int8 NULL,
@@ -7,7 +7,7 @@ CREATE TABLE public.customers (
 );
 
 
-CREATE TABLE public.orders (
+CREATE TABLE kimball.orders (
 	order_id text PRIMARY KEY,
 	customer_id text unique NULL,
 	order_status text NULL,
@@ -24,27 +24,23 @@ CREATE TABLE public.orders (
 
 
 
-CREATE TABLE public.geolocation (
-	geolocation_zip_code_prefix int8 PRIMARY KEY,
+CREATE TABLE kimball.geolocation (
+	geolocation_zip_code_prefix int8 not null ,
 	geolocation_lat float8 NULL,
 	geolocation_lng float8 NULL,
 	geolocation_city text NULL,
 	geolocation_state text NULL
 );
 
-CREATE TABLE public.sellers (
+CREATE TABLE kimball.sellers (
 	seller_id text PRIMARY KEY,
 	seller_zip_code_prefix int8 NULL,
 	seller_city text NULL,
-	seller_state text null,
-	CONSTRAINT fk_seller
-      FOREIGN KEY(seller_zip_code_prefix) 
-	  REFERENCES geolocation(geolocation_zip_code_prefix)
-	  ON DELETE SET NULL
+	seller_state text null
 );
 
 
-CREATE TABLE public.order_itens (
+CREATE TABLE kimball.order_itens (
 	order_id text PRIMARY KEY,
 	order_item_id int8 NULL,
 	product_id text NULL,
@@ -59,11 +55,15 @@ CREATE TABLE public.order_itens (
 	CONSTRAINT fk_seller
       FOREIGN KEY(seller_id) 
 	  REFERENCES sellers(seller_id)
+	  ON DELETE SET null,
+	CONSTRAINT fk_product
+      FOREIGN KEY (product_id) 
+	  REFERENCES products(product_id)
 	  ON DELETE SET NULL
 );
 
 
-CREATE TABLE public.order_reviews (
+CREATE TABLE kimball.order_reviews (
 	review_id text PRIMARY KEY,
 	order_id text NULL,
 	review_score int8 NULL,
@@ -77,7 +77,7 @@ CREATE TABLE public.order_reviews (
 	  ON DELETE SET NULL
 );
 
-CREATE TABLE public.products (
+CREATE TABLE kimball.products (
 	product_id text PRIMARY KEY,
 	product_category_name text NULL,
 	product_name_lenght float8 NULL,
@@ -87,6 +87,7 @@ CREATE TABLE public.products (
 	product_length_cm float8 NULL,
 	product_height_cm float8 NULL,
 	product_width_cm float8 NULL
+
 );
 
 
