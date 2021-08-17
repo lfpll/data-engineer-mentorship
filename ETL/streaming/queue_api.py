d90a24b82209b98eb80c3b2a0bca677cf6aaec70
Luiz Lobo <luizfpll@gmail.com>
2021-08-17 12:16:56 -0300

    adding kafka queue api

---
+++ ETL/streaming/queue_api.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI,Request
+import os
+import json
+from kafka import KafkaProducer
+from typing import List
+
+app = FastAPI()
+
+producer = KafkaProducer(bootstrap_servers=
+                        os.environ['KAFKA_HOST'])
+TOPIC = os.environ['KAFKA_TOPIC']
+
+if isinstance(TOPIC,bytes):
+    TOPIC = TOPIC.decode('utf-8') 
+
+
+@app.post('/')
+async def insert_data(request: Request):
+    
+    resp = await request.json()
+    i = 0
+    for e in resp:
+        i += 1
+        producer.send(topic=TOPIC,
+                      value=bytes(json.dumps(e),encoding='utf-8'))
+    producer.flush()
+    
+    return {"success":"true","num_events":i}
